bf6db93e0a6df4300ea92c560bdf68a73ea316f6
fpem3309 <alsrhks2154@gmail.com>
2022-04-15 20:42:23 +0900

    json console /mainBoard

--- src/App.js
+++ src/App.js
@@ -1,50 +1,35 @@
-// import logo from './logo.svg';
-// import './App.css';
-//
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-//
-// export default App;
-
-
-import React, {Component, useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
-function App () {
-    const [message, setMessage] = useState("");
+function App() {
 
-    useEffect(() => {
-        fetch('/mainProductBoard')
-            .then(response => response.text())
-            .then(message => {
-                setMessage(message);
-            });
-    },[])
+    axios.get("/mainBoard")
+        .then((Response) => {
+            console.log(Response.data)
+        })
+        .catch((Error) => {
+            console.log(Error)
+        });
+
     return (
-<div>
-    {message}
-</div>
-
-    )
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
